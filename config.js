e336a681b116130d4095036c978c32a812e3289b
luisgserrano <luisgserrano.d@gmail.com>
2017-05-27 01:14:07 +0100

    twit lib test

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {};
+
+config.twitter = {
+    consumer_key: "bDFo7BXOHOTl7HuGnjVF4Mfpa",
+    consumer_secret: "ECWDlKZwScSAYzDVzaPvWJNAiUKNb2Nk9YNy9SCEXO7yKbz3P5",
+    access_token: "1274685578-eS51ZpmG4xpAXMJTc0VZaSVZMmjeQ74aayuQdm4",
+    access_token_secret: "7vjzjNdKHPClzSpusFwUMAczv1haArbzRiT7A6PnDWc3O"
+};
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express'),
-      mongoose = require('mongoose'),
-      twitter = require('twit'),
+      Twit = require('twit'),
       config = require('./config');
 
 const app = express(),
       port = process.env.PORT || 8080;
+
+const T = new Twit(config.twitter);
+
+const request = T.stream('statuses/filter', { track: '#golf' })
+
+request.on('tweet', tweet => {
+    console.log(tweet)
+});
