845539235d651ec27d0de3bbad3c98fef241028f
Luis Serrano <luisgserrano.d@gmail.com>
2017-05-28 21:30:43 +0100

    Adding socket.io

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 // Require all dependencies
-const express    = require('express'),
-      http       = require('http'),
-      mongoose   = require('mongoose'),
-      io         = require('socket.io'),
-      Twit       = require('twit'),
-      config     = require('./config'),
-	  routes 	 = require('./routes'),
-	  streamHandler = require('./streamHandler');
+const express       = require('express'),
+      http          = require('http'),
+      mongoose      = require('mongoose'),
+      Twit          = require('twit'),
+      config        = require('./config'),
+      routes        = require('./routes'),
+      streamHandler = require('./streamHandler');
 
 // Create an express instance and set a port variable
 const app  = express(),
@@ -17,7 +16,7 @@ mongoose.connect(config.mongolab);
 
 // Start the server
 const server = http.createServer(app).listen(port, () => {
-      console.log('Express server listening on port' + port);
+      console.log('Express server listening on port ' + port);
 });
 
 // Index route
@@ -28,14 +27,14 @@ app.get('/page/:page/:skip', routes.page);
 
 
 // Set socket.io to listen on server
-io.listen(server);
+let io = require('socket.io')(server);
 
 // Create a new Twit instance
 const T = new Twit(config.twitter);
 
 // Creating a request variable and using stream function from Twit
 // The hashtags we'r listening are #golf and/or #hole19golf
-const request = T.stream('statuses/filter', { track: '#hole19golf, #golf', filter_level: 'medium' });
+const request = T.stream('statuses/filter', { track: '#hole19golf, #golf' });
 
 // Set a stream listener for tweets matching tacking keywords
 request.on('tweet', tweet => {
